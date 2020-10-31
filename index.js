459c7880b9016af6ca5907b1bb1cc9bb630989c1
OthavioH <othavio2650@gmail.com>
2020-10-31 13:26:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -280,8 +280,8 @@ async function connectHLTVBot(matchId){
         
                     if(data.log[0].RoundEnd != undefined){
 
-                        ctScore = data.log[0].RoundEnd.ctTeamScore;
-                        tScore = data.log[0].RoundEnd.tTeamScore;
+                        ctScore = data.log[0].RoundEnd.counterTerroristTeamScore;
+                        tScore = data.log[0].RoundEnd.terroristScore;
 
                         if(ctScore == 0 && tScore == 0){
                             return null;
