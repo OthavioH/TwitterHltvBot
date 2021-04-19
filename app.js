708c1f52a08f73f2afb8a108eefe2b2b7d766ded
OthavioH <othavio2650@gmail.com>
2021-04-18 22:05:24 -0300

    organizing project

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var matchIDRouter = require('./routes/matchID');
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.use(express.static(__dirname + '/src'))
-app.get('/', (req, res) => res.sendFile('./index.html'));
+app.use(express.static(__dirname))
+app.get('/', (req, res) => res.sendFile('./services/index.html'));
 app.use('/matchID',matchIDRouter);
 
 app.listen(port, () => console.log('App listening on port ' + port))
