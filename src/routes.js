4c5409183ddb0141e333e4cc5c2fffd2605136a3
OthavioH <othavio2650@gmail.com>
2021-04-21 00:40:24 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const MatchController = require('./controllers/MatchController');
 
 const routes = express.Router();
 
+routes.get('/',(req,res)=>{res.send("Hello")})
 routes.get('/get_matches',MatchController.index);
 routes.get('/set_match/:matchID',MatchController.connectMatch);
 
